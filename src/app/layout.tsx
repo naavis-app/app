991ade58e33df96f96e115f8c7b912a8a30a270a
noahgerard <helix.dev.xyz@gmail.com>
2024-05-21 16:28:24 -0400

    feat: Add Tagline component to page and remove from global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
             <ClerkProvider>
               <TRPCReactProvider>
                 {<Navbar />}
-                {<Tagline />}
                 {children}
               </TRPCReactProvider>
             </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-// import Link from "next/link";
-// import { api } from "~/trpc/server";
+import Tagline from "./_components/Tagline";
 
 export default async function Home() {
-  return <></>;
+  return (
+    <>
+      <Tagline />
+    </>
+  );
 }
