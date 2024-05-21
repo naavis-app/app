768017ae7a22e3608895a5c3d1114c69dd56b82f
noahgerard <helix.dev.xyz@gmail.com>
2024-05-21 18:23:26 -0400

    HAHAHA MOVING MARKER WOWOW

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import { useEffect, useState } from 'react';
 import { env } from '~/env';
 
 export default function Dashboard() {
-  const position = { lat: 53.54992, lng: 10.00678 };
+  const [position, setPosition] = useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const t = performance.now();
+      const lat = Math.sin(t / 2000) * 5;
+      const lng = Math.cos(t / 3000) * 5;
+
+      setPosition({lat, lng});
+    }, 1000/60);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <>
       <div className="relative flex-1 grow w-full h-full">
         <div className='absolute w-full h-full'>
           <APIProvider apiKey={env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-            <Map mapId={"7342a481321c1609"} defaultCenter={position} defaultZoom={10} streetViewControl={false}>
+            <Map mapId={"7342a481321c1609"} defaultCenter={position} center={position} defaultZoom={8} streetViewControl={false}>
               <AdvancedMarker position={position} title={"test"} />
             </Map>
           </APIProvider>
