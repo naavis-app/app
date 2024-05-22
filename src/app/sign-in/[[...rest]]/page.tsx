851d4226ce57a25d6575dade3cabce4d480ac233
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-21 22:59:14 -0400

    add genstaticparams function for cicd

--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -1,3 +1,7 @@
+export async function generateStaticParams() {
+  return [{ params: {} }];
+}
+
 export default function Page() {
   return (
     <div className="flex h-full w-full flex-1 items-center justify-center">
