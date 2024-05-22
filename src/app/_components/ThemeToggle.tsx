58d69100199183b0b2fb06286bf1c247a5516845
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-22 17:36:54 -0400

    inherit system theme added

--- src/app/_components/ThemeToggle.tsx
+++ src/app/_components/ThemeToggle.tsx
@@ -4,9 +4,15 @@ import { Button } from "@radix-ui/themes";
 import { useAtom } from "jotai";
 import { themeAtom } from "../../server/lib/stores";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useEffect } from "react";
 
 export default function ThemeToggle() {
     const [theme, setTheme] = useAtom(themeAtom);
+
+    useEffect(() => {
+        const query = window.matchMedia("(prefers-color-scheme: dark)");
+        setTheme(query.matches ? "dark" : "light");
+    }, []);
 
     const toggleTheme = () => {
         setTheme(theme === "light" ? "dark" : "light");
