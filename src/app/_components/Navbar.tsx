b47a18c498b25493d8167f9951d027cfda7451c7
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-23 02:01:03 -0400

    chore: prettier formatting

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -46,11 +46,11 @@ export default async function Navbar() {
 
                             {user?.id ? (
                                 <Link
-                                href="/dashboard"
-                                className="hover:no-underline"
-                                >   
+                                    href="/dashboard"
+                                    className="hover:no-underline"
+                                >
                                     <Text
-                                    className="rounded-md border-2
+                                        className="rounded-md border-2
                                     border-blue-500 bg-blue-500
                                     px-4 py-2 font-bold text-white
                                     transition-colors hover:border-blue-700
