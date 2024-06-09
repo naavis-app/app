d55e29b3545c997b26916a61d271903298ebe16f
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-09 19:48:07 -0400

    potential logout fix?

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,12 +2,23 @@
 
 import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { signOut } from "~/server/lib/auth";
 import { userAtom } from "~/server/lib/stores";
 
 export default async function Logout() {
     const [user, setUser] = useAtom(userAtom);
+    const router = useRouter();
 
-    setUser(null);
-    await signOut();
+    useEffect(() => {
+        async function handleLogout() {
+            setUser(null);
+            await signOut();
+            router.push('/');
+        }
+
+        handleLogout();
+    }, [setUser, router]);
+
+    return null;
 }
