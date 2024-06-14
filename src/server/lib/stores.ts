88de90e1a9564a99dc042904a5c627e6545c98b4
noahgerard <helix.dev.xyz@gmail.com>
2024-06-13 20:21:20 -0400

    refactor: themeAtom and userAtom state persist in localstorage

--- src/server/lib/stores.ts
+++ src/server/lib/stores.ts
@@ -5,12 +5,14 @@ this is done using global state management from jotai.
 check https://jotai.org/docs/introduction for more information. */
 
 import { atom } from "jotai";
+import { atomWithStorage } from 'jotai/utils'
+
 import { DatabaseUser, User } from "lucia";
 import { DatabaseUserAttributes } from "./auth";
 import { Device, Group } from "@prisma/client";
 
-export const themeAtom = atom<"light" | "dark" | "inherit">("dark");
-export const userAtom = atom<null | DatabaseUserAttributes>(null);
+export const themeAtom = atomWithStorage<"light" | "dark" | "inherit">('naavis-darkmode', "inherit");
+export const userAtom = atomWithStorage<null | DatabaseUserAttributes>('naavis-usersession', null);
 
 export const sidenavOpenAtom = atom<boolean>(true);
 export const deviceListAtom = atom<Device[]>([]);
