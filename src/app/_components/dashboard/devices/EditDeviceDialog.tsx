7036fc7def2128189150c2d25b240bb87e7ee446
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-17 19:01:38 -0400

    small change

--- src/app/_components/dashboard/devices/EditDeviceDialog.tsx
+++ src/app/_components/dashboard/devices/EditDeviceDialog.tsx
@@ -91,7 +91,7 @@ export default function EditDeviceDialog() {
                                 // onClick={newDevice}
                                 disabled={addingDevice}
                             >
-                                Add Device
+                                Save
                             </Button>
                         </Flex>
                     </Flex>
