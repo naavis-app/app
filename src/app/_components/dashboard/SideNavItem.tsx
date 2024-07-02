50985f31f36b4ae9117bc7d74fa7c733013b8cda
noahgerard <helix.dev.xyz@gmail.com>
2024-07-02 09:29:09 -0400

    feat: Dash sidenav tooltips when collapsed

--- src/app/_components/dashboard/SideNavItem.tsx
+++ src/app/_components/dashboard/SideNavItem.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@radix-ui/themes";
+import { Text, Tooltip } from "@radix-ui/themes";
 import Link from "next/link";
 
 import { motion } from "framer-motion";
@@ -18,24 +18,26 @@ export default function SideNavItem({ label, icon, url }: SideNavItemProps) {
     const pathname = usePathname();
 
     const inner = (
-        <div
-            className={clsx(
-                "flex w-full flex-row items-center gap-2 rounded-md p-4 text-[--accent-11] transition-all ease-in-out hover:cursor-pointer hover:bg-[--accent-3]",
-                !open ? "p-2" : "justify-start pl-8",
-                pathname == url ? " shadow-md" : "",
-            )}
-        >
-            <div className={clsx(!open ? "" : "")}>{icon}</div>
-            <motion.div
-                animate={{
-                    opacity: open ? 1 : 0,
-                    x: open ? 22 : 0,
-                }}
-                className="absolute"
+        <Tooltip content={label} side="right" align="center" className={clsx(open ? "opacity-0" : "")}>
+            <div
+                className={clsx(
+                    "flex w-full flex-row items-center gap-2 rounded-md p-4 text-[--accent-11] transition-all ease-in-out hover:cursor-pointer hover:bg-[--accent-3]",
+                    !open ? "p-2" : "justify-start pl-8",
+                    pathname == url ? " shadow-md" : "",
+                )}
             >
-                <Text>{label}</Text>
-            </motion.div>
-        </div>
+                <div className={clsx(!open ? "" : "")}>{icon}</div>
+                <motion.div
+                    animate={{
+                        opacity: open ? 1 : 0,
+                        x: open ? 22 : 0,
+                    }}
+                    className="absolute"
+                >
+                    <Text>{label}</Text>
+                </motion.div>
+            </div>
+        </Tooltip>
     );
 
     return url ? <Link href={url}>{inner}</Link> : inner;
