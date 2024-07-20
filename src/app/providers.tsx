b320ca8c56fb7bb173d6dbc37a92fa15edbd1056
mustaeen <136145181+must108@users.noreply.github.com>
2024-07-20 00:27:40 -0400

    fix: 'inherit' option for light/dark mode works

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,6 +7,7 @@ import { useAtom } from "jotai";
 import { themeAtom } from "../server/lib/stores";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 interface children {
     children: React.ReactNode;
@@ -14,6 +15,17 @@ interface children {
 
 export function Providers({ children }: { children: React.ReactNode }) {
     const [theme, setTheme] = useAtom(themeAtom);
+
+    useEffect(() => {
+        if (theme === 'inherit') {
+            const inheritedTheme = window.matchMedia("(prefers-color-scheme: dark)");
+            if (inheritedTheme.matches) {
+                setTheme('dark');
+            } else {
+                setTheme('light');
+            }
+        }
+    }, [theme]);
 
     return (
         <ThemeProvider attribute="class">
