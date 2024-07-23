755abecd74ddccaeb4e07dca64cce8e1ba944b2a
must108 <mustaeen18@gmail.com>
2024-07-23 19:14:31 -0400

    fix to window.matchMedia error

--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -4,3 +4,21 @@ import { vi } from "vitest";
 Object.defineProperty(window, "dispatchEvent", {
     value: vi.fn(),
 });
+
+// using window.Matchmedia in app/page.tsx was causing problems with testing.
+// that functionality is needed so the website can have the 
+// correct theme on load, when the theme is set to "inherit"
+// this is how i fixed it:
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(),
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
+// (https://github.com/vitest-dev/vitest/issues/821)
