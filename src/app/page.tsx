36aa00ba95b8a889661d34af26fe769751bc6b93
must108 <mustaeen18@gmail.com>
2024-07-23 19:02:49 -0400

    fixed issue with window.matchMedia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,27 @@
 import Tagline from "./_components/Tagline";
 import { useAtom } from "jotai";
 import { themeAtom } from "~/server/lib/stores";
+import { useEffect } from "react";
 // import { ThemeProvider } from "next-themes";
 import React from "react";
 
 export default function Home() {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [theme, setTheme] = useAtom(themeAtom);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            if (theme === "inherit") {
+                const themeCheck = window.matchMedia("(prefers-color-scheme: dark)");
+        
+                if (themeCheck.matches) {
+                    setTheme("dark");
+                } else {
+                    setTheme("light");
+                }
+            }
+        }
+    }, []);
 
     if (theme === "inherit") {
         const themeCheck = window.matchMedia("(prefers-color-scheme: dark)");
