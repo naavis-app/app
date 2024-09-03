5fa662cd81add38324558c1cc270f61c206806d0
noahgerard <helix.dev.xyz@gmail.com>
2024-09-03 18:14:07 -0400

    feat: CSRF protection (https://lucia-auth.com/guides/validate-session-cookies/nextjs-app)

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+// The following code is from https://lucia-auth.com/guides/validate-session-cookies/nextjs-app
+
+import { verifyRequestOrigin } from "lucia";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+	if (request.method === "GET") {
+		return NextResponse.next();
+	}
+
+	const originHeader = request.headers.get("Origin");
+	// NOTE: You may need to use `X-Forwarded-Host` instead
+	const hostHeader = request.headers.get("Host");
+	if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
+		return new NextResponse(null, {
+			status: 403
+		});
+	}
+
+	return NextResponse.next();
+}
