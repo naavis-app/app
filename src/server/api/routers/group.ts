994b90c1c243b427d7d8ddfcf5236c7f28fca073
noahgerard <helix.dev.xyz@gmail.com>
2024-09-03 18:33:39 -0400

    feat: Added session validation middleware for every trpc endpoint

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -2,277 +2,287 @@ import type { Group, User } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { validateSessionPlugin } from "~/server/lib/plugins";
 import { redis } from "~/server/redis";
 
 // TODO: redis cache optimization and security optimizations
 
 export const groupRouter = createTRPCRouter({
-	create: publicProcedure
-		.input(
-			z.object({
-				name: z.string(),
-				description: z.string(),
-				ownerId: z.string(),
-			}),
-		)
-		.mutation(async ({ ctx, input }) => {
-			try {
-				const newGroup = await ctx.db.group.create({
-					data: {
-						name: input.name,
-						description: input.description,
-						owner: {
-							connect: { id: input.ownerId },
-						},
-						members: {
-							connect: { id: input.ownerId },
-						},
-					},
-				});
+    create: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(
+            z.object({
+                name: z.string(),
+                description: z.string(),
+                ownerId: z.string(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            try {
+                const newGroup = await ctx.db.group.create({
+                    data: {
+                        name: input.name,
+                        description: input.description,
+                        owner: {
+                            connect: { id: input.ownerId },
+                        },
+                        members: {
+                            connect: { id: input.ownerId },
+                        },
+                    },
+                });
 
-				const cacheKey = `group:${newGroup.id}`;
+                const cacheKey = `group:${newGroup.id}`;
 
-				await redis.del(`user:${input.ownerId}:groups`);
-				await redis.setex(cacheKey, 3600, JSON.stringify(newGroup));
+                await redis.del(`user:${input.ownerId}:groups`);
+                await redis.setex(cacheKey, 3600, JSON.stringify(newGroup));
 
-				return newGroup;
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to create group",
-				});
-			}
-		}),
+                return newGroup;
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to create group",
+                });
+            }
+        }),
 
-	read: publicProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				userId: z.string(),
-			}),
-		)
-		.mutation(async ({ ctx, input }) => {
-			const cachedUser = await redis.get(`user:${input.userId}`);
-			let user: User | null;
+    read: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(
+            z.object({
+                id: z.string(),
+                userId: z.string(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            const cachedUser = await redis.get(`user:${input.userId}`);
+            let user: User | null;
 
-			if (cachedUser) {
-				user = JSON.parse(cachedUser);
-			} else {
-				user = await ctx.db.user.findUnique({
-					where: {
-						id: input.userId,
-					},
-				});
-			}
+            if (cachedUser) {
+                user = JSON.parse(cachedUser);
+            } else {
+                user = await ctx.db.user.findUnique({
+                    where: {
+                        id: input.userId,
+                    },
+                });
+            }
 
-			if (!user) {
-				throw new TRPCError({
-					code: "NOT_FOUND",
-					message: "User not found",
-				});
-			}
+            if (!user) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "User not found",
+                });
+            }
 
-			try {
-				const cachedGroup = await redis.get(`group:${input.id}`);
-				let group: Group | null;
+            try {
+                const cachedGroup = await redis.get(`group:${input.id}`);
+                let group: Group | null;
 
-				if (cachedGroup) {
-					group = JSON.parse(cachedGroup);
-				} else {
-					group = await ctx.db.group.findUnique({
-						where: {
-							id: input.id,
-							ownerId: input.userId,
-						},
-					});
-				}
+                if (cachedGroup) {
+                    group = JSON.parse(cachedGroup);
+                } else {
+                    group = await ctx.db.group.findUnique({
+                        where: {
+                            id: input.id,
+                            ownerId: input.userId,
+                        },
+                    });
+                }
 
-				if (!group) {
-					throw new TRPCError({
-						code: "NOT_FOUND",
-						message: "Group was not found!",
-					});
-				}
+                if (!group) {
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "Group was not found!",
+                    });
+                }
 
-				if (group.ownerId !== input.userId) {
-					throw new TRPCError({
-						code: "NOT_FOUND",
-						message: "User is not in this group!",
-					});
-				}
+                if (group.ownerId !== input.userId) {
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "User is not in this group!",
+                    });
+                }
 
-				return group;
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to find group",
-				});
-			}
-		}),
+                return group;
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to find group",
+                });
+            }
+        }),
 
-	update: publicProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				name: z.string(),
-				description: z.string(),
-				userId: z.string(),
-				// TODO: add owner validation
-			}),
-		)
-		.mutation(async ({ ctx, input }) => {
-			try {
-				const updatedGroup = await ctx.db.group.update({
-					where: {
-						id: input.id,
-					},
-					data: {
-						name: input.name,
-						description: input.description,
-					},
-				});
+    update: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(
+            z.object({
+                id: z.string(),
+                name: z.string(),
+                description: z.string(),
+                userId: z.string(),
+                // TODO: add owner validation
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            try {
+                const updatedGroup = await ctx.db.group.update({
+                    where: {
+                        id: input.id,
+                    },
+                    data: {
+                        name: input.name,
+                        description: input.description,
+                    },
+                });
 
-				await redis.del(`group:${input.id}`);
-				await redis.del(`user:${input.userId}:groups`);
+                await redis.del(`group:${input.id}`);
+                await redis.del(`user:${input.userId}:groups`);
 
-				const cacheKey = `group:${updatedGroup.id}`;
-				await redis.setex(cacheKey, 3600, JSON.stringify(updatedGroup));
+                const cacheKey = `group:${updatedGroup.id}`;
+                await redis.setex(cacheKey, 3600, JSON.stringify(updatedGroup));
 
-				return updatedGroup;
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to update group",
-				});
-			}
-		}),
-	getOrCreateInviteCode: publicProcedure
-		.input(z.object({ groupId: z.string() }))
-		.mutation(async ({ ctx, input }) => {
-			try {
-				const group = await ctx.db.group.findUnique({
-					where: {
-						id: input.groupId,
-					},
-				});
+                return updatedGroup;
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to update group",
+                });
+            }
+        }),
+        
+    getOrCreateInviteCode: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(z.object({ groupId: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            try {
+                const group = await ctx.db.group.findUnique({
+                    where: {
+                        id: input.groupId,
+                    },
+                });
 
-				if (!group) {
-					throw new TRPCError({
-						code: "NOT_FOUND",
-						message: "Group not found",
-					});
-				}
+                if (!group) {
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "Group not found",
+                    });
+                }
 
-				if (group?.inviteCode) {
-					return group.inviteCode;
-				}
+                if (group?.inviteCode) {
+                    return group.inviteCode;
+                }
 
-				// TODO: This is not a good way to generate invite codes
-				const inviteCode = Math.random().toString(36).substring(2, 8);
-				await ctx.db.group.update({
-					where: {
-						id: input.groupId,
-					},
-					data: {
-						inviteCode: inviteCode,
-					},
-				});
+                // TODO: This is not a good way to generate invite codes
+                const inviteCode = Math.random().toString(36).substring(2, 8);
+                await ctx.db.group.update({
+                    where: {
+                        id: input.groupId,
+                    },
+                    data: {
+                        inviteCode: inviteCode,
+                    },
+                });
 
-				return inviteCode;
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to get or create invite code",
-				});
-			}
-		}),
-	list: publicProcedure
-		.input(
-			z.object({
-				userId: z.string(),
-			}),
-		)
-		.query(async ({ ctx, input }) => {
-			try {
-				const cachedUser = await redis.get(`user:${input.userId}`);
-				let user: User | null;
+                return inviteCode;
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to get or create invite code",
+                });
+            }
+        }),
 
-				if (cachedUser) {
-					user = JSON.parse(cachedUser);
-				} else {
-					user = await ctx.db.user.findUnique({
-						where: { id: input.userId },
-					});
-				}
+    list: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(
+            z.object({
+                userId: z.string(),
+            }),
+        )
+        .query(async ({ ctx, input }) => {
+            try {
+                const cachedUser = await redis.get(`user:${input.userId}`);
+                let user: User | null;
 
-				if (!user) {
-					throw new TRPCError({
-						code: "NOT_FOUND",
-						message: "User not found",
-					});
-				}
+                if (cachedUser) {
+                    user = JSON.parse(cachedUser);
+                } else {
+                    user = await ctx.db.user.findUnique({
+                        where: { id: input.userId },
+                    });
+                }
 
-				const cacheKey = `user:${input.userId}:groups`;
-				const cachedGroups = await redis.get(cacheKey);
+                if (!user) {
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "User not found",
+                    });
+                }
 
-				if (cachedGroups) {
-					return JSON.parse(cachedGroups);
-				}
+                const cacheKey = `user:${input.userId}:groups`;
+                const cachedGroups = await redis.get(cacheKey);
 
-				const groups = await ctx.db.group.findMany({
-					where: {
-						members: {
-							some: { id: input.userId },
-						},
-					},
-				});
+                if (cachedGroups) {
+                    return JSON.parse(cachedGroups);
+                }
 
-				await redis.setex(cacheKey, 3600, JSON.stringify(groups));
-				return groups;
-				// Find groups that the user is a member of
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to list groups",
-				});
-			}
-		}),
-	delete: publicProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				userId: z.string(),
-				// TODO: add ownerid security.
-			}),
-		)
-		.mutation(async ({ ctx, input }) => {
-			// TODO: AHHHH REMEMBER WE NEED TO PROTECT ALL THESE ENDPOINTS
-			try {
-				await ctx.db.place.deleteMany({
-					where: {
-						groupId: input.id,
-					},
-				});
+                const groups = await ctx.db.group.findMany({
+                    where: {
+                        members: {
+                            some: { id: input.userId },
+                        },
+                    },
+                });
 
-				await redis.del(`group:${input.id}`);
-				await redis.del(`user:${input.userId}:groups`);
+                await redis.setex(cacheKey, 3600, JSON.stringify(groups));
+                return groups;
+                // Find groups that the user is a member of
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to list groups",
+                });
+            }
+        }),
 
-				return await ctx.db.group.delete({
-					where: {
-						id: input.id,
-					},
-				});
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({
-					code: "BAD_REQUEST",
-					message: "Failed to delete group",
-				});
-			}
-		}),
+    delete: publicProcedure
+        .unstable_concat(validateSessionPlugin)
+        .input(
+            z.object({
+                id: z.string(),
+                userId: z.string(),
+                // TODO: add ownerid security.
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            // TODO: AHHHH REMEMBER WE NEED TO PROTECT ALL THESE ENDPOINTS
+            try {
+                await ctx.db.place.deleteMany({
+                    where: {
+                        groupId: input.id,
+                    },
+                });
+
+                await redis.del(`group:${input.id}`);
+                await redis.del(`user:${input.userId}:groups`);
+
+                return await ctx.db.group.delete({
+                    where: {
+                        id: input.id,
+                    },
+                });
+            } catch (e) {
+                console.error(e);
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "Failed to delete group",
+                });
+            }
+        }),
 });
